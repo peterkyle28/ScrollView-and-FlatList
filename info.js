d0e07fab4419860251305571fdb2112b7d82d8af
peterkyle28 <143087590+peterkyle28@users.noreply.github.com>
2023-11-19 19:01:51 +0800

    initial change

---
+++ info.js
@@ -0,0 +1,101 @@
+export const users = [
+    {
+      id:'1',
+      name: 'PETER KYLE D. GINGO',
+      nickName:'Pet',
+      address:'Tip-Tip',
+      courseAndYear:'BSIT-3',
+      avatar:
+        'https://th.bing.com/th/id/OIP.kOfV8ZFOj27_X1DBDqM7IQHaH-?rs=1&pid=ImgDetMain',
+    },
+    {
+        id:'2',
+        name: 'SHEILA G. DELMONTE',
+        nickName:'Sheil',
+        address:'New York',
+        courseAndYear:'EDUCATION4',
+        avatar:
+          'https://p7.hiclipart.com/preview/782/114/405/5bbc3519d674c.jpg',
+      },
+      {
+        id:'3',
+        name: 'PROFILE',
+        nickName:'alpha',
+        address:'Location',
+        courseAndYear:'NA',
+        avatar:
+          'https://p7.hiclipart.com/preview/782/114/405/5bbc3519d674c.jpg',
+      },
+      {
+        id:'4',
+        name: 'PROFILE',
+        nickName:'legend',
+        address:'Location',
+        courseAndYear:'NA',
+        avatar:
+          'https://p7.hiclipart.com/preview/782/114/405/5bbc3519d674c.jpg',
+      },
+      {
+        id:'5',
+        name: 'PROFILE',
+        nickName:'dark',
+        address:'Location',
+        courseAndYear:'NA',
+        avatar:
+          'https://p7.hiclipart.com/preview/782/114/405/5bbc3519d674c.jpg',
+      },
+      {
+        id:'6',
+        name: 'PROFILE',
+        nickName:'kimmie',
+        address:'Location',
+        courseAndYear:'NA',
+        avatar:
+          'https://p7.hiclipart.com/preview/782/114/405/5bbc3519d674c.jpg',
+      },
+      {
+        id:'7',
+        name: 'PROFILE',
+        nickName:'NA',
+        address:'Location',
+        courseAndYear:'NA',
+        avatar:
+          'https://p7.hiclipart.com/preview/782/114/405/5bbc3519d674c.jpg',
+      },
+      {
+        id:'8',
+        name: 'PROFILE',
+        nickName:'NA',
+        address:'Location',
+        courseAndYear:'NA',
+        avatar:
+          'https://p7.hiclipart.com/preview/782/114/405/5bbc3519d674c.jpg',
+      },
+      {
+        id:'9',
+        name: 'PROFILE',
+        nickName:'NA',
+        address:'Location',
+        courseAndYear:'NA',
+        avatar:
+          'https://p7.hiclipart.com/preview/782/114/405/5bbc3519d674c.jpg',
+      },
+      {
+        id:'10',
+        name: 'PROFILE',
+        nickName:'NA',
+        address:'Location',
+        courseAndYear:'NA',
+        avatar:
+          'https://p7.hiclipart.com/preview/782/114/405/5bbc3519d674c.jpg',
+      },
+      {
+        id:'11',
+        name: 'PROFILE',
+        nickName:'NA',
+        address:'Location',
+        courseAndYear:'NA',
+        avatar:
+          'https://p7.hiclipart.com/preview/782/114/405/5bbc3519d674c.jpg',
+      },
+  ];
